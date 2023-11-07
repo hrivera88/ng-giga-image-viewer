346f800775891c75bc7e547ba267ccebeefec0f1
hrivera88 <hrivera@outlook.com>
2023-11-06 22:06:14 -0600

    Updated Tools Modules ZoomTool import

--- src/app/tools/tools.module.ts
+++ src/app/tools/tools.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolsComponent } from './tools.component';
-import { ZoomToolComponent } from '../zoom-tool/zoom-tool.component';
+import { ZoomToolComponent } from './zoom-tool/zoom-tool.component';
 
 @NgModule({
 	declarations: [ToolsComponent, ZoomToolComponent],
